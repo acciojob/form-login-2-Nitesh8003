ec339a8dc34a262c58e7dc08b6b1de3e0b2aee3e
Nitesh8003 <126144227+Nitesh8003@users.noreply.github.com>
2024-03-18 20:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-//your JS code here. If required.
-function showAlert() {
-  alert("First Name Last Name Phone Number Email ID");
+function submitForm() {
+    // Get form data
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var phoneNumber = document.getElementById('phoneNumber').value;
+    var email = document.getElementById('email').value;
+
+    // Construct alert message
+    var alertMessage = "First Name: " + firstName + "\n" +
+                       "Last Name: " + lastName + "\n" +
+                       "Phone Number: " + phoneNumber + "\n" +
+                       "Email ID: " + email;
+
+    // Show alert
+    alert(alertMessage);
 }
